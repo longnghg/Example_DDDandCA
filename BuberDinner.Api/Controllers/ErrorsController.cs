1d6831907864d7a78d6a41ae818106aaf4a39a81
Long Hoang <long.nguyenh@dmchosting.ca>
2024-03-18 15:13:16 +0700

    Traning-Custom problem details factory to handle exception

--- BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -11,6 +11,6 @@ public class ErrorsController : ControllerBase
     public IActionResult Error()
     {
         Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-        return Problem(title:exception.Message ,statusCode:400);
+        return Problem(title:exception?.Message ,statusCode:400);
     }
 }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,7 +1,9 @@
 using BuberDiner.Api.Middleware;
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -10,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
     .AddInfrastructure(builder.Configuration);
     // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 
